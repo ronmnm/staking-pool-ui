b87868f7369c0917b093c02bf35967fedc1bb17f
ronmnm <spellbound.2808@gmail.com>
2020-10-01 11:16:27 +0300

    add github link + escrow abi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react"
 import Header from "./components/header.jsx"
-import Main from "./components/main.jsx"
+import Body from "./components/Body.jsx"
 import web3 from "./web3"
 import { getData } from "./service.js"
 
 function App() {
   let [account, setAccount] = useState("")
+  // let [metamaskConnected, setMetamaskConnected] = useState(false)
   let [data, setData] = useState(null)
   let accounts = []
 
@@ -14,6 +15,7 @@ function App() {
       accounts = await web3.eth.getAccounts()
       if (accounts.length !== 0) {
         setAccount(accounts[0])
+        // setMetamaskConnected(true)
         // console.log("connected")
       }
     })()
@@ -34,8 +36,13 @@ function App() {
 
   return (
     <div className="App">
+      <p>
+        <a className="git_link" href="https://github.com/ronmnm/staking-pool-ui">
+          Github
+        </a>
+      </p>
       <Header account={account} />
-      <Main data={data} setData={setData} account={account} />
+      <Body data={data} setData={setData} account={account} />
     </div>
   )
 }
